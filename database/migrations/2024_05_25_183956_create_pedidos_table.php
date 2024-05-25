1fe177e7a5e6226857615b638f817c98d65441c2
Mirna <mirnarecinos382@gmail.com>
2024-05-25 16:08:17 -0300

    Add database migrations for user and order tables:s´parkles:

---
+++ database/migrations/2024_05_25_183956_create_pedidos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->unsignedBigInteger('user_id');
+            $table->string('product');
+            $table->integer('quantity');
+            $table->float('total');
+            $table->timestamps();
+            // Llave foránea
+            $table->foreign('user_id')->references('id')->on('usuarios')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
