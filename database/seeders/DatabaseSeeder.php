7d4000195715fc1cc5a9612ae88463a9025e20cf
Mirna <mirnarecinos382@gmail.com>
2024-05-26 20:09:29 -0300

    seeders:sparkles:

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Usuarios; 
+use App\Models\Pedidos;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,21 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+       /*  User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]); */
+
+        // utiliza la fábrica del modelo Usuarios para generar los registros y almacenarlos en la variable
+        $usuarios = Usuarios::factory(5)->create();
+      
+       
+         // por cada usuario creado, y almacenado en la variable se crea un pedido y se le asigna el id
+         foreach ($usuarios as $usuario) {
+            Pedidos::factory()->create(['user_id' => $usuario->id]);
+        }
+
+
+
     }
 }
