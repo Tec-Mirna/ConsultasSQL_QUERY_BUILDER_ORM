45881e7d65796822514f4cfe2f2aade938e92bdd
Mirna <mirnarecinos382@gmail.com>
2024-05-26 20:08:14 -0300

    Model update:sparkles:

--- app/Models/Pedidos.php
+++ app/Models/Pedidos.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Usuarios;
 
 class Pedidos extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id', 
+        'product', 
+        'quantity', 
+        'total'
+    ];
+
+    // indica relación  de tablas, cada pedido pertenece a un usuario
+    public function usuario()
+    { //belongsTo relación inversa
+        return $this->belongsTo(Usuarios::class, 'user_id');
+    }
 }
--- app/Models/Usuarios.php
+++ app/Models/Usuarios.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Pedidos;
 
 class Usuarios extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'email', 
+        'phone'
+    ];
+
+    // Indica relación entre tablas
+    public function pedidos()
+    { // el método hasMany  define una relación uno a muchos.
+        return $this->hasMany(Pedidos::class, 'user_id'); //hasMany  Un usuario puede tener muchos pedidos
+    }
 }
