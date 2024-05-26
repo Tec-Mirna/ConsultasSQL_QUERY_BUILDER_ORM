6550610230394532f2989c8bf8484704961be08d
Mirna <mirnarecinos382@gmail.com>
2024-05-26 20:10:26 -0300

    routes:sparkles:

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PedidosController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/ejercicio1', [PedidosController::class, 'getPedidosByUserId']);
+Route::get('/ejercicio2', [PedidosController::class, 'getPedidosDetalles']);
+Route::get('/ejercicio3', [PedidosController::class, 'getPedidosRango']);
+Route::get('/ejercicio4', [PedidosController::class, 'getUsuariosConR']);
+Route::get('/ejercicio5', [PedidosController::class, 'calcularTotalPedidosUsuario']);
+Route::get('/ejercicio6', [PedidosController::class, 'getAll']);
+Route::get('/ejercicio7', [PedidosController::class, 'sumTotal']);
+Route::get('/ejercicio8', [PedidosController::class, 'pedidoEconomico']);
+Route::get('/ejercicio9', [PedidosController::class, 'pedidosAgrupadosPorUsuario']);
+
+
